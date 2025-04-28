19b9b9b2f845f2cc7f4e3832caee7db0a881038f
Pahang Krisdyan <pahangkrisdyan@gmail.com>
2025-04-29 00:31:51 +0700

    Add missing @RegisterForReflection to SpoomeUrlShortenerServiceResponse and remove unused imports

--- src/main/java/io/pahangkrisdyan/mcp/server/tool/urlshotener/SpoomeUrlShortenerServiceResponse.java
+++ src/main/java/io/pahangkrisdyan/mcp/server/tool/urlshotener/SpoomeUrlShortenerServiceResponse.java
@@ -2,8 +2,8 @@ package io.pahangkrisdyan.mcp.server.tool.urlshotener;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.validation.constraints.Max;
-import org.hibernate.validator.constraints.URL;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
+@RegisterForReflection
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record SpoomeUrlShortenerServiceResponse(String shortUrl){}
